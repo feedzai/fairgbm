6e0d745d7f0dab5596fba79a536d6209a2887a07
GoncaloArsenio <goncalo.ars@gmail.com>
2022-07-26 11:37:02 +0100

    Uncomment all matplotlib related tests

--- tests/python_package_test/test_plotting.py
+++ tests/python_package_test/test_plotting.py
@@ -70,47 +70,47 @@ def test_plot_importance(params, breast_cancer_split, train_data):
     assert ax2.patches[3].get_facecolor() == (0, 0, 1., 1.)  # b
 
 
-# @pytest.mark.skipif(not MATPLOTLIB_INSTALLED, reason='matplotlib is not installed')
-# def test_plot_split_value_histogram(params, breast_cancer_split, train_data):
-#     X_train, _, y_train, _ = breast_cancer_split
+@pytest.mark.skipif(not MATPLOTLIB_INSTALLED, reason='matplotlib is not installed')
+def test_plot_split_value_histogram(params, breast_cancer_split, train_data):
+    X_train, _, y_train, _ = breast_cancer_split
 
-#     gbm0 = lgb.train(params, train_data, num_boost_round=10)
-#     ax0 = lgb.plot_split_value_histogram(gbm0, 27)
-#     assert isinstance(ax0, matplotlib.axes.Axes)
-#     assert ax0.get_title() == 'Split value histogram for feature with index 27'
-#     assert ax0.get_xlabel() == 'Feature split value'
-#     assert ax0.get_ylabel() == 'Count'
-#     assert len(ax0.patches) <= 2
+    gbm0 = lgb.train(params, train_data, num_boost_round=10)
+    ax0 = lgb.plot_split_value_histogram(gbm0, 27)
+    assert isinstance(ax0, matplotlib.axes.Axes)
+    assert ax0.get_title() == 'Split value histogram for feature with index 27'
+    assert ax0.get_xlabel() == 'Feature split value'
+    assert ax0.get_ylabel() == 'Count'
+    assert len(ax0.patches) <= 2
 
-#     gbm1 = lgb.LGBMClassifier(n_estimators=10, num_leaves=3, silent=True)
-#     gbm1.fit(X_train, y_train)
+    gbm1 = lgb.LGBMClassifier(n_estimators=10, num_leaves=3, silent=True)
+    gbm1.fit(X_train, y_train)
 
-#     ax1 = lgb.plot_split_value_histogram(gbm1, gbm1.booster_.feature_name()[27], figsize=(10, 5),
-#                                          title='Histogram for feature @index/name@ @feature@',
-#                                          xlabel='x', ylabel='y', color='r')
-#     assert isinstance(ax1, matplotlib.axes.Axes)
-#     title = 'Histogram for feature name {}'.format(gbm1.booster_.feature_name()[27])
-#     assert ax1.get_title() == title
-#     assert ax1.get_xlabel() == 'x'
-#     assert ax1.get_ylabel() == 'y'
-#     assert len(ax1.patches) <= 2
-#     for patch in ax1.patches:
-#         assert patch.get_facecolor() == (1., 0, 0, 1.)  # red
+    ax1 = lgb.plot_split_value_histogram(gbm1, gbm1.booster_.feature_name()[27], figsize=(10, 5),
+                                         title='Histogram for feature @index/name@ @feature@',
+                                         xlabel='x', ylabel='y', color='r')
+    assert isinstance(ax1, matplotlib.axes.Axes)
+    title = 'Histogram for feature name {}'.format(gbm1.booster_.feature_name()[27])
+    assert ax1.get_title() == title
+    assert ax1.get_xlabel() == 'x'
+    assert ax1.get_ylabel() == 'y'
+    assert len(ax1.patches) <= 2
+    for patch in ax1.patches:
+        assert patch.get_facecolor() == (1., 0, 0, 1.)  # red
 
-#     ax2 = lgb.plot_split_value_histogram(gbm0, 27, bins=10, color=['r', 'y', 'g', 'b'],
-#                                          title=None, xlabel=None, ylabel=None)
-#     assert isinstance(ax2, matplotlib.axes.Axes)
-#     assert ax2.get_title() == ''
-#     assert ax2.get_xlabel() == ''
-#     assert ax2.get_ylabel() == ''
-#     assert len(ax2.patches) == 10
-#     assert ax2.patches[0].get_facecolor() == (1., 0, 0, 1.)  # r
-#     assert ax2.patches[1].get_facecolor() == (.75, .75, 0, 1.)  # y
-#     assert ax2.patches[2].get_facecolor() == (0, .5, 0, 1.)  # g
-#     assert ax2.patches[3].get_facecolor() == (0, 0, 1., 1.)  # b
+    ax2 = lgb.plot_split_value_histogram(gbm0, 27, bins=10, color=['r', 'y', 'g', 'b'],
+                                         title=None, xlabel=None, ylabel=None)
+    assert isinstance(ax2, matplotlib.axes.Axes)
+    assert ax2.get_title() == ''
+    assert ax2.get_xlabel() == ''
+    assert ax2.get_ylabel() == ''
+    assert len(ax2.patches) == 10
+    assert ax2.patches[0].get_facecolor() == (1., 0, 0, 1.)  # r
+    assert ax2.patches[1].get_facecolor() == (.75, .75, 0, 1.)  # y
+    assert ax2.patches[2].get_facecolor() == (0, .5, 0, 1.)  # g
+    assert ax2.patches[3].get_facecolor() == (0, 0, 1., 1.)  # b
 
-#     with pytest.raises(ValueError):
-#         lgb.plot_split_value_histogram(gbm0, 0)  # was not used in splitting
+    with pytest.raises(ValueError):
+        lgb.plot_split_value_histogram(gbm0, 0)  # was not used in splitting
 
 
 # @pytest.mark.skipif(not MATPLOTLIB_INSTALLED or not GRAPHVIZ_INSTALLED,
@@ -163,39 +163,39 @@ def test_plot_importance(params, breast_cancer_split, train_data):
 #     assert 'count' not in graph_body
 
 
-# @pytest.mark.skipif(not MATPLOTLIB_INSTALLED, reason='matplotlib is not installed')
-# def test_plot_metrics(params, breast_cancer_split, train_data):
-#     X_train, X_test, y_train, y_test = breast_cancer_split
-#     test_data = lgb.Dataset(X_test, y_test, reference=train_data)
-#     params.update({"metric": {"binary_logloss", "binary_error"}})
+@pytest.mark.skipif(not MATPLOTLIB_INSTALLED, reason='matplotlib is not installed')
+def test_plot_metrics(params, breast_cancer_split, train_data):
+    X_train, X_test, y_train, y_test = breast_cancer_split
+    test_data = lgb.Dataset(X_test, y_test, reference=train_data)
+    params.update({"metric": {"binary_logloss", "binary_error"}})
 
-#     evals_result0 = {}
-#     lgb.train(params, train_data,
-#               valid_sets=[train_data, test_data],
-#               valid_names=['v1', 'v2'],
-#               num_boost_round=10,
-#               evals_result=evals_result0,
-#               verbose_eval=False)
-#     ax0 = lgb.plot_metric(evals_result0)
-#     assert isinstance(ax0, matplotlib.axes.Axes)
-#     assert ax0.get_title() == 'Metric during training'
-#     assert ax0.get_xlabel() == 'Iterations'
-#     assert ax0.get_ylabel() in {'binary_logloss', 'binary_error'}
-#     ax0 = lgb.plot_metric(evals_result0, metric='binary_error')
-#     ax0 = lgb.plot_metric(evals_result0, metric='binary_logloss', dataset_names=['v2'])
+    evals_result0 = {}
+    lgb.train(params, train_data,
+              valid_sets=[train_data, test_data],
+              valid_names=['v1', 'v2'],
+              num_boost_round=10,
+              evals_result=evals_result0,
+              verbose_eval=False)
+    ax0 = lgb.plot_metric(evals_result0)
+    assert isinstance(ax0, matplotlib.axes.Axes)
+    assert ax0.get_title() == 'Metric during training'
+    assert ax0.get_xlabel() == 'Iterations'
+    assert ax0.get_ylabel() in {'binary_logloss', 'binary_error'}
+    ax0 = lgb.plot_metric(evals_result0, metric='binary_error')
+    ax0 = lgb.plot_metric(evals_result0, metric='binary_logloss', dataset_names=['v2'])
 
-#     evals_result1 = {}
-#     lgb.train(params, train_data,
-#               num_boost_round=10,
-#               evals_result=evals_result1,
-#               verbose_eval=False)
-#     with pytest.raises(ValueError):
-#         lgb.plot_metric(evals_result1)
+    evals_result1 = {}
+    lgb.train(params, train_data,
+              num_boost_round=10,
+              evals_result=evals_result1,
+              verbose_eval=False)
+    with pytest.raises(ValueError):
+        lgb.plot_metric(evals_result1)
 
-#     gbm2 = lgb.LGBMClassifier(n_estimators=10, num_leaves=3, silent=True)
-#     gbm2.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=False)
-#     ax2 = lgb.plot_metric(gbm2, title=None, xlabel=None, ylabel=None)
-#     assert isinstance(ax2, matplotlib.axes.Axes)
-#     assert ax2.get_title() == ''
-#     assert ax2.get_xlabel() == ''
-#     assert ax2.get_ylabel() == ''
+    gbm2 = lgb.LGBMClassifier(n_estimators=10, num_leaves=3, silent=True)
+    gbm2.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=False)
+    ax2 = lgb.plot_metric(gbm2, title=None, xlabel=None, ylabel=None)
+    assert isinstance(ax2, matplotlib.axes.Axes)
+    assert ax2.get_title() == ''
+    assert ax2.get_xlabel() == ''
+    assert ax2.get_ylabel() == ''

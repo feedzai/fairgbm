2142d92337a93569de313b655427016813a46367
GoncaloArsenio <goncalo.ars@gmail.com>
2022-07-26 14:50:05 +0100

    Change back to ubuntu; comment graphviz tests

--- tests/python_package_test/test_plotting.py
+++ tests/python_package_test/test_plotting.py
@@ -113,54 +113,54 @@ def test_plot_split_value_histogram(params, breast_cancer_split, train_data):
         lgb.plot_split_value_histogram(gbm0, 0)  # was not used in splitting
 
 
-@pytest.mark.skipif(not MATPLOTLIB_INSTALLED or not GRAPHVIZ_INSTALLED,
-                    reason='matplotlib or graphviz is not installed')
-def test_plot_tree(breast_cancer_split):
-    X_train, _, y_train, _ = breast_cancer_split
-    gbm = lgb.LGBMClassifier(n_estimators=10, num_leaves=3, silent=True)
-    gbm.fit(X_train, y_train, verbose=False)
+# @pytest.mark.skipif(not MATPLOTLIB_INSTALLED or not GRAPHVIZ_INSTALLED,
+#                     reason='matplotlib or graphviz is not installed')
+# def test_plot_tree(breast_cancer_split):
+#     X_train, _, y_train, _ = breast_cancer_split
+#     gbm = lgb.LGBMClassifier(n_estimators=10, num_leaves=3, silent=True)
+#     gbm.fit(X_train, y_train, verbose=False)
 
-    with pytest.raises(IndexError):
-        lgb.plot_tree(gbm, tree_index=83)
+#     with pytest.raises(IndexError):
+#         lgb.plot_tree(gbm, tree_index=83)
 
-    ax = lgb.plot_tree(gbm, tree_index=3, figsize=(15, 8), show_info=['split_gain'])
-    assert isinstance(ax, matplotlib.axes.Axes)
-    w, h = ax.axes.get_figure().get_size_inches()
-    assert int(w) == 15
-    assert int(h) == 8
+#     ax = lgb.plot_tree(gbm, tree_index=3, figsize=(15, 8), show_info=['split_gain'])
+#     assert isinstance(ax, matplotlib.axes.Axes)
+#     w, h = ax.axes.get_figure().get_size_inches()
+#     assert int(w) == 15
+#     assert int(h) == 8
 
 
-@pytest.mark.skipif(not GRAPHVIZ_INSTALLED, reason='graphviz is not installed')
-def test_create_tree_digraph(breast_cancer_split):
-    X_train, _, y_train, _ = breast_cancer_split
+# @pytest.mark.skipif(not GRAPHVIZ_INSTALLED, reason='graphviz is not installed')
+# def test_create_tree_digraph(breast_cancer_split):
+#     X_train, _, y_train, _ = breast_cancer_split
 
-    constraints = [-1, 1] * int(X_train.shape[1] / 2)
-    gbm = lgb.LGBMClassifier(n_estimators=10, num_leaves=3, silent=True, monotone_constraints=constraints)
-    gbm.fit(X_train, y_train, verbose=False)
+#     constraints = [-1, 1] * int(X_train.shape[1] / 2)
+#     gbm = lgb.LGBMClassifier(n_estimators=10, num_leaves=3, silent=True, monotone_constraints=constraints)
+#     gbm.fit(X_train, y_train, verbose=False)
 
-    with pytest.raises(IndexError):
-        lgb.create_tree_digraph(gbm, tree_index=83)
+#     with pytest.raises(IndexError):
+#         lgb.create_tree_digraph(gbm, tree_index=83)
 
-    graph = lgb.create_tree_digraph(gbm, tree_index=3,
-                                    show_info=['split_gain', 'internal_value', 'internal_weight'],
-                                    name='Tree4', node_attr={'color': 'red'})
-    graph.render(view=False)
-    assert isinstance(graph, graphviz.Digraph)
-    assert graph.name == 'Tree4'
-    assert graph.filename == 'Tree4.gv'
-    assert len(graph.node_attr) == 1
-    assert graph.node_attr['color'] == 'red'
-    assert len(graph.graph_attr) == 0
-    assert len(graph.edge_attr) == 0
-    graph_body = ''.join(graph.body)
-    assert 'leaf' in graph_body
-    assert 'gain' in graph_body
-    assert 'value' in graph_body
-    assert 'weight' in graph_body
-    assert '#ffdddd' in graph_body
-    assert '#ddffdd' in graph_body
-    assert 'data' not in graph_body
-    assert 'count' not in graph_body
+#     graph = lgb.create_tree_digraph(gbm, tree_index=3,
+#                                     show_info=['split_gain', 'internal_value', 'internal_weight'],
+#                                     name='Tree4', node_attr={'color': 'red'})
+#     graph.render(view=False)
+#     assert isinstance(graph, graphviz.Digraph)
+#     assert graph.name == 'Tree4'
+#     assert graph.filename == 'Tree4.gv'
+#     assert len(graph.node_attr) == 1
+#     assert graph.node_attr['color'] == 'red'
+#     assert len(graph.graph_attr) == 0
+#     assert len(graph.edge_attr) == 0
+#     graph_body = ''.join(graph.body)
+#     assert 'leaf' in graph_body
+#     assert 'gain' in graph_body
+#     assert 'value' in graph_body
+#     assert 'weight' in graph_body
+#     assert '#ffdddd' in graph_body
+#     assert '#ddffdd' in graph_body
+#     assert 'data' not in graph_body
+#     assert 'count' not in graph_body
 
 
 @pytest.mark.skipif(not MATPLOTLIB_INSTALLED, reason='matplotlib is not installed')

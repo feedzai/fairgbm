ad180bbc2979f6acfeb2552f37e5476f1e946284
AndreFCruz <andrecruz97@gmail.com>
2022-09-02 16:39:21 +0100

    adding proxy losses to cmake target

--- include/LightGBM/proxy_losses/proxy_loss_factory.hpp
+++ include/LightGBM/proxy_losses/proxy_loss_factory.hpp
@@ -10,34 +10,15 @@
 #ifndef LIGHTGBM_PROXY_LOSSES_PROXY_LOSS_FACTORY_HPP_
 #define LIGHTGBM_PROXY_LOSSES_PROXY_LOSS_FACTORY_HPP_
 
-#include "base.hpp"
-#include "hinge.hpp"
-#include "xentropy.hpp"
-#include "quadratic.hpp"
+#include <LightGBM/proxy_losses/base.hpp>
+#include <LightGBM/config.h>
 
 #include <string>
 
 namespace LightGBM {
 namespace Constrained {
 
-    std::unique_ptr<ProxyLoss> ConstructProxyLoss(const Config &config)
-    {
-        std::string stepwise_proxy = config.constraint_stepwise_proxy;
-        if (stepwise_proxy == "hinge") {
-            return std::unique_ptr<HingeProxyLoss>(new HingeProxyLoss((score_t) config.stepwise_proxy_margin));
-        }
-//        else if (stepwise_proxy == "cross_entropy")
-//        {
-//            return std::unique_ptr<CrossEntropyProxyLoss>(new CrossEntropyProxyLoss(proxy_margin));
-//        }
-//        else if (stepwise_proxy == "quadratic")
-//        {
-//            return std::unique_ptr<QuadraticProxyLoss>(new QuadraticProxyLoss(proxy_margin));
-//        }
-        else {
-            throw std::invalid_argument("constraint_stepwise_proxy=" + stepwise_proxy + " not implemented!");
-        }
-    }
+    std::unique_ptr<ProxyLoss> ConstructProxyLoss(const LightGBM::Config &config);
 
 }   // Constrained
 }   // LightGBM
